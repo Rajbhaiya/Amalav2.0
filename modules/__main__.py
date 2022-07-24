7d940d2c5f7d25ffc46536c54aa1048daf9d63fa
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-24 22:50:01 +0530

    Update __main__.py

--- modules/__main__.py
+++ modules/__main__.py
@@ -6,10 +6,6 @@ from modules.clientbot import call_py, bot, user
 async def start_bot():
     await bot.start()
     print("[INFO]: BOT & UBOT CLIENT STARTED !!")
-    await call_py.start()
-    print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
-    await user.join_chat("telugucoders")
-    await user.join_chat("tgshadow_fighters")
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()

46be5a6736ac720df4668d06063b980d7f75edb2
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-24 22:29:34 +0530

    Update __main__.py

--- modules/__main__.py
+++ modules/__main__.py
@@ -1,6 +1,5 @@
 from pyrogram import idle
 from pyrogram import Client as Bot
-from modules.clientbot import run
 from modules.config import API_ID, API_HASH, BOT_TOKEN
 from pytgcalls import PyTgCalls, idle
 

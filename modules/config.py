e6146f95112256e6e8a6810d13b56b79be7f056c
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-08-02 12:29:47 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,7 +1,9 @@
-import os
-import aiohttp
+import re
+import sys
 from os import getenv
+
 from dotenv import load_dotenv
+from pyrogram import filters
     
 load_dotenv()
 que = {}
@@ -21,6 +23,19 @@ IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
 IMG_5 = getenv("IMG_5", "https://telegra.ph/file/d08d6474628be7571f013.png") 
 aiohttpsession = aiohttp.ClientSession()
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(
+        int(x) * 60**i
+        for i, x in enumerate(reversed(stringt.split(":")))
+    )
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(
+    time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00")
+)
 if YOUTUBE_IMG_URL:
     if YOUTUBE_IMG_URL != "assets/Youtube.jpeg":
         if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
